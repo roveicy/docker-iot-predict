3fb5290f8b84fd4de7d46f984811b6be9d598760
Papan Das <hexoindia@gmail.com>
2019-08-22 00:47:03 +0530

    Data Field added

--- python/addSensor.py
+++ python/addSensor.py
@@ -4,7 +4,7 @@ import json
 url = "http://ec2-54-146-44-86.compute-1.amazonaws.com:3000/composer/admin/addSensor"
 headers = {'content-type': "application/json", 'cache-control': "no-cache"}
 
-payload = '{"device":"3","ts":"98977966695555","seq":"DD","dsize":"1024","dhash":"########"}'
+payload = '{"device":"3","ts":"98977966695555","seq":"DD","data":"ddd","dsize":"1024","dhash":"########"}'
 
 response = requests.request("POST", url, data=payload, headers=headers)
 
